f4b505d8329cc22862cfe8ea71a2d420e5737cdf
kadench <kadench@byui.edu>
2024-02-06 17:28:39 -0700

    Fixed incorrect error

--- math.js
+++ math.js
@@ -2,8 +2,9 @@ document.addEventListener("DOMContentLoaded", function() {
     var questionElement = document.getElementById("question");
     var answerElement = document.getElementById("answer");
     var buttonElement = document.getElementById("check-answer");
-    var feedbackElement = document.getElementById("feedback"); // Ensure this element is in your HTML
-    var currentAttempt = 0; // Track the number of attempts
+    var feedbackElement = document.getElementById("feedback");
+    var currentAttempt = 0;
+    var correctAnswer = generateQuestion(); // Initialize and assign correct answer to global variable
 
     function generateQuestion() {
         var types = ["addition", "multiplication", "division", "subtraction"];
@@ -12,25 +13,12 @@ document.addEventListener("DOMContentLoaded", function() {
 
         switch (type) {
             case "addition":
-                if (Math.random() < .09) {
-                    a = Math.floor(Math.random() * 900) + 100; // 100 to 999
-                    b = Math.floor(Math.random() * 900) + 100; // 100 to 999
-                }
-                else {
-                    a = Math.floor(Math.random() * 90) + 10; // 100 to 999
-                    b = Math.floor(Math.random() * 90) + 10; // 100 to 999
-                }
-                answer = a + b;
+                a = Math.random() < 0.09 ? Math.floor(Math.random() * 900) + 100 : Math.floor(Math.random() * 90) + 10;
+                b = Math.random() < 0.09 ? Math.floor(Math.random() * 900) + 100 : Math.floor(Math.random() * 90) + 10;
                 break;
             case "multiplication":
-                if (Math.random() < 0.5) { // 50% chance for 'a' or 'b' to be a three-digit number ending in 0
-                    a = (Math.floor(Math.random() * 90) + 1) * 1;
-                    b = Math.floor(Math.random() * 9) + 1;
-                } else {
-                    a = Math.floor(Math.random() * 9) + 1;
-                    b = (Math.floor(Math.random() * 90) + 1) * 1;
-                }
-                answer = a * b;
+                a = Math.random() < 0.5 ? (Math.floor(Math.random() * 90) + 1) : Math.floor(Math.random() * 9) + 1;
+                b = Math.random() < 0.5 ? Math.floor(Math.random() * 9) + 1 : (Math.floor(Math.random() * 90) + 1);
                 break;                
             case "division":
                 b = Math.floor(Math.random() * 12) + 1;
@@ -38,23 +26,28 @@ document.addEventListener("DOMContentLoaded", function() {
                 a = b * answer;
                 break;
             case "subtraction":
-                if (Math.random() < .09) {
-                    a = Math.floor(Math.random() * 900) + 100;
-                    b = Math.floor(Math.random() * (a - 1)) + 1;
-                }
-                else {
-                    a = Math.floor(Math.random() * 90) + 100;
-                    b = Math.floor(Math.random() * (a - 1)) + 1;
-                }
-                
-                answer = a - b;
+                a = Math.random() < 0.09 ? Math.floor(Math.random() * 900) + 100 : Math.floor(Math.random() * 90) + 100;
+                b = Math.floor(Math.random() * (a - 1)) + 1;
                 break;
         }
+
+        answer = evaluateAnswer(a, b, type); // Calculate answer based on operation
         questionElement.textContent = `What is ${a} ${type === 'addition' ? '+' : type === 'multiplication' ? '*' : type === 'division' ? '/' : '-'} ${b}?`;
         return answer;
     }
 
-    var correctAnswer = generateQuestion();
+    function evaluateAnswer(a, b, type) {
+        switch (type) {
+            case "addition":
+                return a + b;
+            case "multiplication":
+                return a * b;
+            case "division":
+                return a / b;
+            case "subtraction":
+                return a - b;
+        }
+    }
 
     answerElement.addEventListener("input", function() {
         buttonElement.style.backgroundColor = this.value ? "#395a96" : "gray";
@@ -63,54 +56,50 @@ document.addEventListener("DOMContentLoaded", function() {
 
     document.getElementById("math-form").addEventListener("keypress", function(e) {
         if (e.key === "Enter") {
-            e.preventDefault(); // Prevent the form from submitting
+            e.preventDefault();
             buttonElement.click();
         }
     });
 
     buttonElement.addEventListener("click", function(e) {
         e.preventDefault();
-        var userAnswer = parseFloat(answerElement.value);
-        showFeedback(userAnswer === correctAnswer, correctAnswer);
-    
-        // Diagnostic log to verify the condition is met
-        console.log("User Answer: ", userAnswer, "Correct Answer: ", correctAnswer, "Current Attempt: ", currentAttempt);
+        var userAnswer = answerElement.value;
+        if (parseFloat(userAnswer) === parseFloat(answerElement.value)) {
+            userAnswer = parseFloat(userAnswer)
+        }
+        showFeedback(userAnswer === correctAnswer, userAnswer, correctAnswer);
     
         if (userAnswer === correctAnswer) {
-            console.log("Redirecting to YouTube in 1 second...");
             setTimeout(function() {
-                console.log("Redirect now!");
-                // Explicitly using window.location to navigate
-                window.location = "https://www.youtube.com";
-            }, 1000); // Delay in milliseconds
+                window.location.replace("https://www.youtube.com");
+            }, 1000);
         } else {
             currentAttempt++;
             if (currentAttempt === 2) {
-                console.log("Redirecting to YouTube Kids in 1 second...");
                 setTimeout(function() {
-                    console.log("Redirect to YouTube Kids now!");
-                    window.location = "https://www.youtubekids.com";
-                }, 1000); // Delay in milliseconds
+                    window.location.replace("https://www.youtubekids.com");
+                }, 1000);
             }
             correctAnswer = generateQuestion();
-            answerElement.value = ""; // Clear the input field
+            answerElement.value = "";
         }
-    });        
-    function showFeedback(isCorrect, answer) {
-        feedbackElement.style.opacity = 0; // Reset opacity to ensure transition effect
-        feedbackElement.style.visibility = 'hidden'; // Initially hidden for transition
-        setTimeout(() => { // Delay to allow CSS to apply
-            feedbackElement.textContent = isCorrect ? `Correct! The answer was ${answer}!` : `Incorrect! The answer was not ${answer}.` + (currentAttempt === 2 ? " Redirecting." : " Try again.");
+    });
+
+    function showFeedback(isCorrect, canswer, answer) {
+        feedbackElement.style.opacity = 0;
+        feedbackElement.style.visibility = 'hidden';
+        setTimeout(() => {
+            feedbackElement.textContent = isCorrect ? `Correct! The answer was ${answer}!` : `Incorrect! The answer was not ${canswer}.` + (currentAttempt === 2 ? " Redirecting." : " Try again.");
             feedbackElement.className = isCorrect ? 'correct' : 'incorrect';
             feedbackElement.style.visibility = 'visible';
             feedbackElement.style.opacity = 1;
             setTimeout(() => {
-                feedbackElement.style.opacity = 0; // Fade out after 3 seconds
+                feedbackElement.style.opacity = 0;
                 setTimeout(() => {
                     feedbackElement.className = 'hidden';
-                    feedbackElement.style.visibility = 'hidden'; // Hide after fade out
-                }, 300); // Wait for opacity transition
+                    feedbackElement.style.visibility = 'hidden';
+                }, 300);
             }, 3000);
-        }, 10); // Small delay to ensure CSS transition applies
+        }, 10);
     }    
 });
